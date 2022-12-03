8c75ff8f35963e3c845f3f1439da107ce9cdb03e
Yoshimura3 <kimoogle009@gmail.com>
2022-12-03 15:34:29 +0900

    chage https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if(\APP::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
+
     }
 }
